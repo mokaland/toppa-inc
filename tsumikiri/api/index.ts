0893d9b1af2d98c56188373137703da6ad993401
mokaland <sugawara.tomokazu1996@gmail.com>
2026-02-15 19:03:47 +0900

    [Engineer] Cloudflare Workers (Hono) API基盤を構築

---
+++ tsumikiri/api/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+
+const app = new Hono();
+
+app.use('*', cors());
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
+
+// TODO: /api/chat, /api/report, /api/document のルーティングを追加
+
+export default app;
