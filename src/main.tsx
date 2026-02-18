bcf9f01d5e0be24c7870bcb614010b2e7c98afed
mokaland <sugawara.tomokazu1996@gmail.com>
2026-02-18 17:00:50 +0900

    [Product Lead] Fix: App.tsxのインポートパスを修正しCIビルド失敗に対応

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-// 作成者: CTO マルコ・ロッシ
-// 日付: 2026-02-17
-// ステータス: 提案
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './components/App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
